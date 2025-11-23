0c2cccecb926bf728d5ec9f939b7bf79df6069e3
codertheashish <coderashishthe@gmail.com>
2025-11-23 08:01:26 +0530

    Add files via upload

---
+++ QR_code.py
@@ -0,0 +1,13 @@
+import qrcode
+
+print("Enter your URL (Google Drive link or any link):")
+data = input("Paste URL here: ")
+
+# QR generate
+qr = qrcode.make(data)
+
+# Save
+file_name = "generated_qr.png"
+qr.save(file_name)
+
+print(f"Your QR Code is ready ✅ Saved as: {file_name}")
